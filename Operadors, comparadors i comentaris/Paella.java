05401351fa70eb0cdb7d99ab7bf0bbe114939eed
AaronPeva <aaronpeva@gmail.com>
2025-10-01 19:47:22 +0200

    Cmabios

--- Operadors, comparadors i comentaris/Paella.java
+++ Operadors, comparadors i comentaris/Paella.java
@@ -1,39 +1,29 @@
 import java.util.Scanner;
-
+import java.util.Locale;
 class Paella {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.ENGLISH);
         
-        int C = sc.nextInt(); // Comensals cantidad
-        float A = sc.nextFloat(); // Arroz precio kg
-        float G = sc.nextFloat(); // Gambas precio kg
+        int C = sc.nextInt();
+        float A = sc.nextFloat(); 
+        float G = sc.nextFloat(); 
         
-        // Calcular cantidades necesarias
-        // Por cada 4 personas: 0.5 kg arroz, 0.25 kg gambas
+     
         float kg_arroz = (C / 4.0f) * 0.5f;
         float kg_gambas = (C / 4.0f) * 0.25f;
         
-        // Calcular costos
+    
         float costo_arroz = kg_arroz * A;
         float costo_gambas = kg_gambas * G;
         float costo_total = costo_arroz + costo_gambas;
         
-        // Imprimir resultados con formato correcto
-        System.out.println(formatNumber(kg_arroz) + " kg arros");
-        System.out.println(formatNumber(kg_gambas) + " kg gambes");
-        System.out.println(formatNumber(costo_arroz) + " euros arros");
-        System.out.println(formatNumber(costo_gambas) + " euros gambes");
-        System.out.println("TOTAL: " + formatNumber(costo_total) + " euros");
+        System.out.println(kg_arroz + " kg arros");
+        System.out.println(kg_gambas + " kg gambes");
+        System.out.println(costo_arroz + " euros arros");
+        System.out.println(costo_gambas + " euros gambes");
+        System.out.println("TOTAL: " + costo_total + " euros");
         
         sc.close();
-    }
-    
-    // Función para formatear números (eliminar .0 innecesario)
-    public static String formatNumber(float num) {
-        if (num == (int)num) {
-            return String.valueOf((int)num);
-        } else {
-            return String.valueOf(num);
-        }
     }
 }
