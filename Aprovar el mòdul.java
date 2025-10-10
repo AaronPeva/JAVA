b19dfddcf6103928b43c082b2fa72345b6fcfc41
AaronPeva <aaronpeva@gmail.com>
2025-10-10 19:05:19 +0200

    html

---
+++ Aprovar el mòdul.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class aprovar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int P1 = sc.nextInt();
+        int P2 = sc.nextInt();
+        int P3 = sc.nextInt();
+        int P4 = sc.nextInt();
+        int P5 = sc.nextInt();
+        int P6 = sc.nextInt();
+        int P7 = sc.nextInt();
+        int P8 = sc.nextInt();
+        int P9 = sc.nextInt();
+
+        P1
+        boolean UF1 = P3 >= 7 ||  
+        System.out.print(r);
+        sc.close();
+    }
+}
